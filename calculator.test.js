4d41d6463a8ff2a3eefabcb4a3f499f8c8d2761e
Nassim <nsmsbr03@gmail.com>
2025-04-15 17:10:18 +0200

    nasssss03

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,4 @@
-const calculator = require('./calculator');
-
+onst calculator = require('./calculator');
 // Passing Test: Addizione (2 + 2 = 4)
 test('should correctly add two numbers', () => {
   const result = calculator.addizione(2, 2);
@@ -7,7 +6,7 @@ test('should correctly add two numbers', () => {
 });
 
 // Failing Test: Addizione (3 + 5 ≠ 7)
-test('should fail when adding two numbers incorrectly', () => {
+test.skip('should fail when adding two numbers incorrectly', () => {
   const result = calculator.addizione(3, 5);
   expect(result).toBe(7); // Assertion: This will fail because 3 + 5 = 8, not 7
 });
